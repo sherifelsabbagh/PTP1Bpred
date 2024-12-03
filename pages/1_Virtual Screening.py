5c378d98b5df9a767c0365addb7534ef45806127
Sherif Elsabbagh <60114574+sherifelsabbagh@users.noreply.github.com>
2024-12-03 22:05:03 +0000

    jjkj

--- pages/1_Virtual Screening.py
+++ pages/1_Virtual Screening.py
@@ -26,7 +26,7 @@ def PUbchemfp_desc_calc():
     output, error = process.communicate()
     os.remove('molecule.smi')
 
-st.cache_data(clear_cache=True)
+@st.cache_data(clear_cache=True)
     
 def descriptors(smiles):
     mols = [Chem.MolFromSmiles(s) for s in smiles] 
